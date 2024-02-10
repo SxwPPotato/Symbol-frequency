a135956141a6e06ed8bb977ae13e25a45751a35d
Vitalya <vitalyabes.vb@gmail.com>
2024-02-10 10:57:49 +0300

    Initial commit

---
+++ Symbo_ frequency.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+bool pred(const std::pair< int, int >& a, const std::pair< int, int >& b)
+{
+    return a.second > b.second;
+}
+
+
+int main()
+{
+    std::map<char, int> mp;
+    std::string str = "Hello world!!";
+
+    for (auto i : str) ++mp[i];
+    std::vector< std::pair< char, int > > vec(mp.begin(), mp.end());
+    std::sort(vec.begin(), vec.end(), pred);
+
+    for (auto p : vec) 
+        std::cout << p.first << ' ' << p.second << std::endl;
+
+
+    return 0;
+
+}
+
